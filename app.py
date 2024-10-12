dcb2ef52a2a26cba518cbde8fe5e48034cfd06b7
Nima Ahmadpour <163693351+Nima-A936@users.noreply.github.com>
2024-10-12 13:26:21 +0330

    Update app.py

--- app.py
+++ app.py
@@ -123,7 +123,7 @@ img2 = get_img_as_base64("Pictures/E-Commerce.png")
 page_bg_img = f"""
 <style>
 [data-testid="stAppViewContainer"] > .main {{
-background-image: url("data:image/png;base64,{img1}");
+background-image: url("data:image/jpg;base64,{img}");
 background-size: cover;
 background-position: top left;
 background-repeat: no-repeat;
