3c4bde90ef441985ccdcce008e2d8517fccf477a
Nima Ahmadpour <163693351+Nima-A936@users.noreply.github.com>
2024-10-12 14:48:50 +0330

    Update app.py

--- app.py
+++ app.py
@@ -110,12 +110,12 @@ img2 = get_img_as_base64("Pictures/E-Commerce.png")
 # CSS to apply the background image
 page_bg_img = f"""
 <style>
-[data-testid="stAppViewContainer"] > .main {
+[data-testid="stAppViewContainer"] > .main {{
     background-image: url("https://images.unsplash.com/photo-1542281286-9e0a16bb7366");
     background-size: 100vw 100vh;  # This sets the size to cover 100% of the viewport width and height
     background-position: center;  
     background-repeat: no-repeat;
-}
+}}
 
 [data-testid="stSidebar"] > div:first-child {{
 background-image: url("data:image/jpg;base64,{img}");
