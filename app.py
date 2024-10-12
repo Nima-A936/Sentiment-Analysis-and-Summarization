f99304938f7aaf0cc2e3f414dc68739a5f121b4b
Nima Ahmadpour <163693351+Nima-A936@users.noreply.github.com>
2024-10-12 12:23:30 +0330

    Update app.py

--- app.py
+++ app.py
@@ -107,6 +107,37 @@ img1 = get_img_as_base64("Pictures/Applications.png")
 img2 = get_img_as_base64("Pictures/E-Commerce.png")
 
 
+##
+@st.cache(allow_output_mutation=True)
+def get_base64_of_bin_file(bin_file):
+    with open(bin_file, 'rb') as f:
+        data = f.read()
+    return base64.b64encode(data).decode()
+
+def set_jpg_as_page_bg(jpg_file):
+    bin_str = get_base64_of_bin_file(jpg_file)
+    page_bg_img = '''
+    <style>
+    body {
+    background-image: url("data:image/jpg;base64,%s");
+    background-size: cover;
+    background-repeat: no-repeat;
+    background-attachment: fixed;
+    }
+    </style>
+    ''' % bin_str
+    
+    st.markdown(page_bg_img, unsafe_allow_html=True)
+    return
+set_jpg_as_page_bg('desktop.jpg')
+##
+
+
+
+
+
+
+
 # CSS to apply the background image
 page_bg_img = f"""
 <style>
